9238d499778c6b45841c77fb55951f96f10cb841
JohnDoe <jtc.cavalheiro@gmail.com>
2021-03-10 20:27:59 +0000

    update

--- API_youtube.py
+++ API_youtube.py
@@ -34,7 +34,7 @@ def ytvideos(channel, order):
         'part': 'snippet',
         'channelId': channel,
         'order' : order,
-        'key': 'AIzaSyArRcAIHTRS6wUkpALur6n2K3FnXfAvZcI'
+        'key': 'your key'
     }
 
     api_result = requests.get('https://www.googleapis.com/youtube/v3/search', params)
